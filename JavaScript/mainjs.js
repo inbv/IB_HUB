ccea2c945efad07415c0eade9b738785538ac9be
INBV <68478358+inbv@users.noreply.github.com>
2021-03-10 23:43:43 -0600

    first commit

---
+++ JavaScript/mainjs.js
@@ -0,0 +1,20 @@
+/*$(document).ready(function() {
+    $(window).scroll( function(){
+        $('#articles-window-container').each( function(i){
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1'},500);
+            }
+        }); 
+    });
+});*/
+
+/*jQuery(window).scroll(function () {
+    var y = jQuery(this).scrollTop();
+    if (y > 100) {
+    jQuery('#articles-window-container').fadeIn(600);
+    } else {
+    jQuery('#articles-window-container').fadeOut(600);
+    }
+    });*/
